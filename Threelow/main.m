5509e9671abe3d2bfd00962fc3af1abee27739fe
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-04-11 10:55:41 -0400

    Players can now roll

--- Threelow/main.m
+++ Threelow/main.m
@@ -19,17 +19,31 @@ int main(int argc, const char * argv[])
         Dice *dice4 = [[Dice alloc] init];
         Dice *dice5 = [[Dice alloc] init];
     
-        [dice1 randomizeValue];
-        [dice2 randomizeValue];
-        [dice3 randomizeValue];
-        [dice4 randomizeValue];
-        [dice5 randomizeValue];
-        
-        NSLog(@"Dice 1 : %ld ",(long)dice1.m_CurrentValue);
-        NSLog(@"Dice 2 : %ld ",(long)dice2.m_CurrentValue);
-        NSLog(@"Dice 3 : %ld ",(long)dice3.m_CurrentValue);
-        NSLog(@"Dice 4 : %ld ",(long)dice4.m_CurrentValue);
-        NSLog(@"Dice 5 : %ld ",(long)dice5.m_CurrentValue);
+        while(true)
+        {
+            NSLog(@"TYPE 'roll' to Roll your 5 dice");
+            char inputChars[255];
+            fgets(inputChars,255,stdin);
+            
+            NSString *userInput = [NSString stringWithCString:inputChars encoding:NSUTF8StringEncoding];
+            
+            userInput = [userInput stringByTrimmingCharactersInSet: NSCharacterSet.whitespaceAndNewlineCharacterSet];
+            
+            if([userInput isEqualToString:(@"roll")])
+            {
+                [dice1 randomizeValue];
+                [dice2 randomizeValue];
+                [dice3 randomizeValue];
+                [dice4 randomizeValue];
+                [dice5 randomizeValue];
+                
+                NSLog(@"Dice 1 : %ld ",(long)dice1.m_CurrentValue);
+                NSLog(@"Dice 2 : %ld ",(long)dice2.m_CurrentValue);
+                NSLog(@"Dice 3 : %ld ",(long)dice3.m_CurrentValue);
+                NSLog(@"Dice 4 : %ld ",(long)dice4.m_CurrentValue);
+                NSLog(@"Dice 5 : %ld ",(long)dice5.m_CurrentValue);
+            }
+        }
     }
     return 0;
 }
